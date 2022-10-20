cf53ca8e43e88d48db45fcb98f9fde0ee4c5a6b4
mailkaze <mailkaze@gmail.com>
2022-10-19 22:59:51 -0400

    animaciones

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,18 +1,51 @@
-import 'package:fl_components/theme/app_theme.dart';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context) {
+  void displayIOSDialog(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('titulo'),
+          content: Column(mainAxisSize: MainAxisSize.min, children: const [
+            Text('este es el contenido de la alerta'),
+            SizedBox(
+              height: 10,
+            ),
+            FlutterLogo(
+              size: 100,
+            )
+          ]),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancelar')),
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('ok')),
+          ],
+        );
+      },
+    );
+  }
+
+  void displayAndroidDialog(BuildContext context) {
     showDialog(
         barrierDismissible: true,
         context: context,
         builder: ((context) {
           return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
             elevation: 5,
-            title: Text('titulo'),
-            content: Column(mainAxisSize: MainAxisSize.min, children: [
+            title: const Text('titulo'),
+            content: Column(mainAxisSize: MainAxisSize.min, children: const [
               Text('este es el contenido de la alerta'),
               SizedBox(
                 height: 10,
@@ -21,7 +54,11 @@ class AlertScreen extends StatelessWidget {
                 size: 100,
               )
             ]),
-            actions: [],
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cerrar'))
+            ],
           );
         }));
   }
@@ -38,13 +75,13 @@ class AlertScreen extends StatelessWidget {
             style: TextStyle(fontSize: 16),
           ),
         ),
-        onPressed: () => displayDialog(context),
+        onPressed: () => Platform.isAndroid
+            ? displayAndroidDialog(context)
+            : displayIOSDialog(context),
       )),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
---
+++ lib/screens/avatar_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AvatarScreen extends StatelessWidget {
+  const AvatarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Avatars'),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 5),
+            child: CircleAvatar(
+              backgroundColor: Colors.indigo[900],
+              child: const Text('SL'),
+            ),
+          )
+        ],
+      ),
+      body: const Center(
+        child: CircleAvatar(
+          maxRadius: 110,
+          backgroundImage: AssetImage('assets/profile.png'),
+        ),
+      ),
+    );
+  }
+}
